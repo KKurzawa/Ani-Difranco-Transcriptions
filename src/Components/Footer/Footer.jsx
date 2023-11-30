5903a7a1292b66ade8c77b4c743e741f8b424bdf
Kris Kurzawa <kurzawa1@yahoo.com>
2023-11-29 20:32:11 -0500

    finished footer, resolving vercel bug

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,20 @@
 import './Footer.css'
+import { FaFacebook } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
 
 function Footer() {
     return (
-        <div className='footer flex justify-center bg-black text-white h-40'>
-            <h2 className='m-10 text-5xl'>Footer</h2>
-        </div>
+        <footer className='bg-black text-white h-48 flex flex-col'>
+            <div className='footer flex justify-around items-center mb-2 '>
+                <a href='https://www.facebook.com/natalie.siagkrisseymour.1' target="_blank" rel="noreferrer"><FaFacebook className='text-6xl md:ml-60 lg:ml-80' /></a>
+                <a href='https://anidifranco.com/' target="_blank" rel="noreferrer"><img className='w-28 -rotate-1' src='./footerLogo.png'></img></a>
+                <a href='https://www.instagram.com/natshapednat/' target="_blank" rel="noreferrer"><FaInstagram className='text-6xl md:mr-60 lg:mr-80' /></a>
+            </div>
+            <div>
+                <a href='https://immense-everglades-74554-f9bad473782a.herokuapp.com/' target="_blank" rel="noreferrer"><h4 className='flex justify-end items-end mt-4 mr-6 mb-2 text-xs'>Site Designed and Built By Kris Kurzawa</h4></a>
+            </div>
+        </footer>
+
     )
 }
 
--- src/Pages/AboutMe/AboutMe.jsx
+++ src/Pages/AboutMe/AboutMe.jsx
@@ -1,5 +1,5 @@
 import './AboutMe.css';
-import natPic from '../../assets/photos/natPic.jpg'
+import natPic from '../../assets/photos/natpic.jpg'
 
 function AboutMe() {
     return (
@@ -8,7 +8,7 @@ function AboutMe() {
             <div className='flex justify-center mt-5'>
                 <img src={natPic} />
             </div>
-            <p className='font-mono mx-40 my-10 text-center text-4xl text-black'>
+            <p className='font-mono mx-40 my-10 text-center text-xl md:text-2xl text-black'>
                 I am a big fan of Ani Difranco. I am a big fan of Ani Difranco.  I am a big fan of Ani Difranco.  I am a big fan of Ani Difranco.  I am a big fan of Ani Difranco.  I am a big fan of Ani Difranco.  I am a big fan of Ani Difranco.  I am a big fan of Ani Difranco.  I am a big fan of Ani Difranco.
             </p>
         </>
