fff499c8d6347bffd583b2a18ac531c366d825e1
Kris Kurzawa <kurzawa1@yahoo.com>
2023-12-18 15:23:23 -0500

    updated body of home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,8 +7,9 @@ function Home() {
         <div className='bg-[#c2b39c]'>
             <img className='w-full' src={AniPic2} />
             <section>
-                <p className='home-description-text mx-10 md:mx-40 mt-10 pb-10 text-center text-2xl md:text-4xl text-[#412720]'>the ultimate unauthorized fan-made and researched collection of the unique musical compositions of <span className='text-[#4F7942]'>ani difranco</span>
+                <p className='home-description-text mx-10 md:mx-40 mt-10 text-center pb-1 text-2xl md:text-4xl  text-[#412720]'>the ultimate unauthorized fan-made and researched collection of the unique musical compositions of
                 </p>
+                <div className='home-ad text-[#4F7942] text-center pb-10 text-2xl md:text-4xl'>ani difranco</div>
             </section>
 
         </div>
