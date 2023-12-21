78f14f33a7719ad5a97c67af8e6e1ddc72712773
Kris Kurzawa <kurzawa1@yahoo.com>
2023-12-20 19:54:47 -0500

    updated footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,14 +4,14 @@ import { FaInstagram } from "react-icons/fa";
 
 function Footer() {
     return (
-        <footer className='footer2 bg-[#1d110e] text-[#f2dcb3] h-28 flex flex-col'>
-            <div className='footer flex justify-around items-center '>
-                <a href='https://www.facebook.com/natalie.siagkrisseymour.1' target="_blank" rel="noreferrer"><FaFacebook className='text-6xl md:ml-60 lg:ml-80 hover:opacity-80' /></a>
-                <a href='https://anidifranco.com/' target="_blank" rel="noreferrer"><img className='w-44 -rotate-1 hover:opacity-80' src='./footerLogo2.png'></img></a>
-                <a href='https://www.instagram.com/natshapednat/' target="_blank" rel="noreferrer"><FaInstagram className='text-6xl md:mr-60 lg:mr-80 hover:opacity-80' /></a>
+        <footer className='footer2 bg-[#1d110e] text-[#f2dcb3] flex flex-col'>
+            <div className='footer flex justify-around items-end h-20'>
+                <a href='https://www.facebook.com/natalie.siagkrisseymour.1' target="_blank" rel="noreferrer"><FaFacebook className='mb-2 md:mb-0 text-5xl md:text-6xl md:ml-60 lg:ml-80 hover:opacity-80' /></a>
+                <a href='https://anidifranco.com/' target="_blank" rel="noreferrer" className='md:-mb-2'><img className='ani-logo w-24 md:w-28 -rotate-1 hover:opacity-80' src='./footerLogo2.png'></img></a>
+                <a href='https://www.instagram.com/natshapednat/' target="_blank" rel="noreferrer"><FaInstagram className='mb-2 md:mb-0 text-5xl md:text-6xl md:mr-60 lg:mr-80 hover:opacity-80' /></a>
             </div>
             <div>
-                <a href='https://immense-everglades-74554-f9bad473782a.herokuapp.com/' target="_blank" rel="noreferrer"><h4 className='footer-description-text flex justify-end items-end mt-1 md:mt-0 mr-2 mb-1 text-[.85em] md:text-[1em]'>site designed and built by kris kurzawa</h4></a>
+                <a href='https://immense-everglades-74554-f9bad473782a.herokuapp.com/' target="_blank" rel="noreferrer" className='w-64'><h4 className='footer-description-text flex justify-end items-end mr-2 pb-1 text-[.6em] md:text-[.85em]'>site designed and built by kris kurzawa</h4></a>
             </div>
         </footer>
 
