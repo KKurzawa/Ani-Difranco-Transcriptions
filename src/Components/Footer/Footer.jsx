d9a134b688cd5b9e35a95c911105e7d686c3ce07
Kris Kurzawa <kurzawa1@yahoo.com>
2023-12-20 22:51:32 -0500

    added youTube Liink

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,14 @@
 import './Footer.css'
 import { FaFacebook } from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa";
+import { FaInstagram, FaYoutube } from "react-icons/fa";
+
 
 function Footer() {
     return (
         <footer className='footer2 bg-[#1d110e] text-[#f2dcb3] flex flex-col'>
             <div className='footer flex justify-around items-end h-20'>
-                <a href='https://www.facebook.com/natalie.siagkrisseymour.1' target="_blank" rel="noreferrer"><FaFacebook className='mb-2 md:mb-0 text-5xl md:text-6xl md:ml-60 lg:ml-80 hover:opacity-80' /></a>
+                <a href='https://www.youtube.com/' target="_blank" rel="noreferrer"><FaYoutube className='mb-2 w-40 md:mb-0 text-5xl md:text-6xl md:ml-60 lg:ml-80 hover:opacity-80' /></a>
+                <a href='https://www.facebook.com/natalie.siagkrisseymour.1' target="_blank" rel="noreferrer"><FaFacebook className='mb-2 md:mb-0 text-5xl md:text-6xl hover:opacity-80' /></a>
                 <a href='https://anidifranco.com/' target="_blank" rel="noreferrer" className='mb-1 md:-mb-2'><img className='ani-logo w-20 md:w-28 -rotate-1 hover:opacity-80' src='./footerLogo2.png'></img></a>
                 <a href='https://www.instagram.com/natshapednat/' target="_blank" rel="noreferrer"><FaInstagram className='mb-2 md:mb-0 text-5xl md:text-6xl md:mr-60 lg:mr-80 hover:opacity-80' /></a>
             </div>
